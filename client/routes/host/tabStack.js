fa5e90bbcafd5295dfdb7793ecd3be9f6969b5ea
Lav Senghani <49972875+lavlife06@users.noreply.github.com>
2020-10-11 16:50:22 +0530

    Update tabStack.js

--- client/routes/host/tabStack.js
+++ client/routes/host/tabStack.js
@@ -1,58 +1,51 @@
-import React, { useEffect } from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AntDesign } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import ProfileStack from "./otherStack/profileStack";
-import HomeStack from "./otherStack/homeStack";
-import UploadPostModal from "../../screens/host/postHandling/uploadPost";
-import Pushnotification from "../../screens/host/pushnotification";
+import React, { useEffect } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import ProfileStack from './otherStack/profileStack';
+import HomeStack from './otherStack/homeStack';
+import UploadPostModal from '../../screens/host/postHandling/uploadPost';
 
 const Tab = createBottomTabNavigator();
 
 const UploadPostComponent = () => null;
 
 export default function TabStack() {
+  
   const showTabIcons = (route, focused, color, size) => {
-    if (route.name === "Home") {
+    if (route.name === 'Home') {
       return (
-        <AntDesign name="home" size={30} color={focused ? "#4ecca3" : "gray"} />
+        <AntDesign name="home" size={30} color={focused ? '#4ecca3' : 'gray'} />
       );
-    } else if (route.name === "Search") {
+    } else if (route.name === 'Search') {
       return (
         <FontAwesome
           name="search"
           size={30}
-          color={focused ? "#4ecca3" : "gray"}
+          color={focused ? '#4ecca3' : 'gray'}
         />
       );
-    } else if (route.name === "Profile") {
+    } else if (route.name === 'Profile') {
       return (
         <MaterialCommunityIcons
           name="face-profile"
           size={30}
-          color={focused ? "#4ecca3" : "gray"}
+          color={focused ? '#4ecca3' : 'gray'}
         />
       );
-    } else if (route.name === "Event") {
+    } else if (route.name === 'Event') {
       return (
         <MaterialIcons
           name="event"
           size={30}
-          color={focused ? "#4ecca3" : "gray"}
+          color={focused ? '#4ecca3' : 'gray'}
         />
-      );
-    } else if (route.name === "Notification") {
-      return (
-        <MaterialIcons
-          name="notifications"
-          size={30}
-          color={focused ? "#4ecca3" : "gray"}
-        />
-      );
+      )
     }
   };
+
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
@@ -60,24 +53,23 @@ export default function TabStack() {
           showTabIcons(route, focused, color, size),
       })}
       tabBarOptions={{
-        activeTintColor: "#4ecca3",
-        inactiveTintColor: "gray",
+        activeTintColor: '#4ecca3',
+        inactiveTintColor: 'gray',
         showLabel: false,
         style: {
-          backgroundColor: "#232931",
-        },
+          backgroundColor: '#232931'
+        }
       }}
     >
       <Tab.Screen name="Home" component={HomeStack} />
-      <Tab.Screen
-        name="Upload"
+      <Tab.Screen 
+        name="Upload" 
         options={({ navigation, route }) => ({
-          tabBarIcon: () => <UploadPostModal navigation={navigation} />,
+          tabBarIcon: () => (<UploadPostModal navigation={navigation} />),
         })}
-        component={UploadPostComponent}
+        component={UploadPostComponent} 
       />
       <Tab.Screen name="Profile" component={ProfileStack} />
-      <Tab.Screen name="Notification" component={Pushnotification} />
     </Tab.Navigator>
   );
 }
