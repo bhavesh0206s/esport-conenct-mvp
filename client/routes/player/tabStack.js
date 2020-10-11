5e6479b49e7e3dda994b8e4c7ac6c3d1f488869d
Lav Senghani <49972875+lavlife06@users.noreply.github.com>
2020-10-11 16:52:04 +0530

    Update tabStack.js

--- client/routes/player/tabStack.js
+++ client/routes/player/tabStack.js
@@ -1,47 +1,47 @@
-import React, { useEffect } from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AntDesign } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import EventStack from "./otherStack/eventStack";
-import ProfileStack from "./otherStack/profileStack";
-import HomeStack from "./otherStack/homeStack";
-import NotificationStack from "./otherStack/notificationStack";
-import { Icon } from "react-native-elements";
+import React, { useEffect } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import EventStack from './otherStack/eventStack'
+import ProfileStack from './otherStack/profileStack';
+import HomeStack from './otherStack/homeStack';
+import NotificationStack from './otherStack/notificationStack';
+import { Icon } from 'react-native-elements';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabStack() {
+  
   const showTabIcons = (route, focused, color, size) => {
-    if (route.name === "Home") {
+    if (route.name === 'Home') {
       return (
-        <AntDesign name="home" size={30} color={focused ? "#4ecca3" : "gray"} />
+        <AntDesign name="home" size={30} color={focused ? '#4ecca3' : 'gray'} />
       );
-    } else if (route.name === "Notification") {
+    } else if (route.name === 'Notification') {
       return (
-        <MaterialIcons
-          name="notifications-none"
+        <MaterialIcons 
+          name="notifications-none" 
           size={30}
-          color={focused ? "#4ecca3" : "gray"}
-        />
+          color={focused ? '#4ecca3' : 'gray'}/>
       );
-    } else if (route.name === "Profile") {
+    } else if (route.name === 'Profile') {
       return (
         <MaterialCommunityIcons
           name="face-profile"
           size={30}
-          color={focused ? "#4ecca3" : "gray"}
+          color={focused ? '#4ecca3' : 'gray'}
         />
       );
-    } else if (route.name === "Event") {
+    } else if (route.name === 'Event') {
       return (
         <MaterialIcons
           name="event"
           size={30}
-          color={focused ? "#4ecca3" : "gray"}
+          color={focused ? '#4ecca3' : 'gray'}
         />
-      );
+      )
     }
   };
 
@@ -52,9 +52,9 @@ export default function TabStack() {
           showTabIcons(route, focused, color, size),
       })}
       tabBarOptions={{
-        activeTintColor: "#4ecca3",
-        inactiveTintColor: "gray",
-        showLabel: false,
+        activeTintColor: '#4ecca3',
+        inactiveTintColor: 'gray',
+        showLabel: false
       }}
     >
       <Tab.Screen name="Home" component={HomeStack} />
