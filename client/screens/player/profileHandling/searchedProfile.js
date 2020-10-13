5dc666884dfb682d42dbf522a60a8f1a126b115f
lavlife06 <lav7990192369@gmail.com>
2020-10-13 14:38:27 +0530

    added some new components for profile

--- client/screens/player/profileHandling/searchedProfile.js
+++ client/screens/player/profileHandling/searchedProfile.js
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, ImageBackground  } from 'react-native';
-import { Avatar, Button, Text } from 'react-native-elements';
-import { useDispatch, useSelector } from 'react-redux';
-import Loading from '../../../shared/loading';
-import SearchedUserTabView from './searchedUsertabView';
-import SearchedHostTabView from './searchedHostTabView ';
-import { AntDesign } from '@expo/vector-icons';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Loading from "../../../shared/loading";
+import CommonSearchedProfile from "../../../components/profilehandling/commonsearchedProfile";
 
 const SearchedUserProfile = ({ navigation, route }) => {
   const dispatch = useDispatch();
@@ -21,79 +17,13 @@ const SearchedUserProfile = ({ navigation, route }) => {
   } else {
     return (
       <>
-        <View style={styles.container}>
-          <ImageBackground 
-            source={require('../../../assets/coverProfile.jpg')} 
-            style={styles.image} 
-          >
-            <View style={styles.content}>
-              <Avatar
-                size={80}
-                rounded
-                overlayContainerStyle={{ backgroundColor: "black" }}
-                icon={{ name: "user", type: "font-awesome-5", color:'#fff' }}
-                activeOpacity={1}
-                containerStyle={styles.avatar}
-              />
-              <View style={{ alignItems: "center" }}>
-                <Text style={{ fontSize: 15, color: '#95bdb5' }}>
-                  ({' '}{particularUser.username ? particularUser.username : ''}{' '})
-                </Text>
-              </View>
-              <View style={{ alignItems: "center" }}>
-                <Text style={{ fontSize: 25, fontWeight:'bold' }}>{particularUser.name}</Text>
-              </View>
-            </View>
-          </ImageBackground>
-          <View style={styles.about}>
-            <Text style={{ fontSize: 14, color: "gray"}}>
-              About:{" "}
-            </Text>
-            <Text style={{ fontSize: 16, color: '#95bdb5' }}>
-              {particularUser.bio ? particularUser.bio : "Please fill this pepole want to know about you"}
-            </Text>
-          </View>
-        </View>
-        {isHostProfile ? <SearchedHostTabView /> : <SearchedUserTabView />}
+        <CommonSearchedProfile
+          particularUser={particularUser}
+          isHostProfile={isHostProfile}
+        />
       </>
     );
   }
 };
 
-const styles = StyleSheet.create({
-  container: {
-    height: 220,
-  },  
-  content:{
-    paddingTop: 100,
-    backgroundColor:'rgba(40, 59, 53,0.7)',
-    paddingBottom: 20, 
-    flex: 1 
-  },  
-  avatar: {
-    position: "absolute",
-    alignSelf: 'center',
-    top: 20
-  },
-  about:{
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 10,
-    paddingBottom: 20
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    
-  },
-  edit: {
-    position:'absolute',
-    padding: 10,
-    alignSelf: 'flex-end'
-  }
-});
-
 export default SearchedUserProfile;
-
