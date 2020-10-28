baf12dd282c473a9d6caea6e9355661817b5673b
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-10-28 17:21:21 +0530

    implemented basic notification

--- client/screens/player/home.js
+++ client/screens/player/home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, FlatList, RefreshControl } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "../../shared/loading";
@@ -7,9 +7,24 @@ import Event from "./EventHandling/event";
 import setAuthToken from "../../Redux/setAuthToken";
 import AsyncStorage from "@react-native-community/async-storage";
 import { getCurrentProfile } from "../../Redux/actions/profile";
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const Home = ({ navigation }) => {
   const dispatch = useDispatch();
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
   const { allEvents, loading } = useSelector((state) => ({
     allEvents: state.event.allEvents,
     loading: state.loading,
@@ -23,8 +38,39 @@ const Home = ({ navigation }) => {
     setRefreshing(false);
   };
 
+  const registerForPushNotificationsAsync = async () => {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
+
   useEffect(() => {
-    const loadHome = async () => {
+    (async () => {
       console.log("Home Page refreshed");
       const token = await AsyncStorage.getItem("token");
       if (token !== null) {
@@ -34,8 +80,21 @@ const Home = ({ navigation }) => {
         dispatch(fetchallEvents());
         dispatch(getCurrentProfile());
       }, 100);
+    })();
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
     };
-    loadHome();
   }, []);
 
   if (loading) {
--- client/screens/player/notifcationHandling/notification.js
+++ client/screens/player/notifcationHandling/notification.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { View } from "react-native";
-import { Image, Text } from 'react-native-elements';
+import { Image, Text,Button } from 'react-native-elements';
+import * as Notifications from 'expo-notifications';
 
 const Notification = () => {
+  const schedulePushNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 1 },
+    });
+  }
   return (
     <View>
-      <Text style={{fontSize: 30, textAlign: 'center', marginTop: 50}}>No Notification</Text>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
     </View>
   );
 }
