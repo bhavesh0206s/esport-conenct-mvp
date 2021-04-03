f0398ace4cdaaaa0dd865be5475ecb7f2060ed57
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2021-04-03 21:02:19 +0530

    remove key.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  jwtSecret: "fjsadfsdlfkleofvdvmtuy5343skvmjfkj",
-  googleClientID: '467702790820-s0u6sis67ec5qejumveermfkqfov16mq.apps.googleusercontent.com',
-  googleClentSecret: 'RSNNmQVy2fyyI1_2W9XxGE6z',
-  mongoURI_dev: 'mongodb+srv://esport-connect:esportconnect@esport-dev-tnq7y.azure.mongodb.net/test?retryWrites=true&w=majority',
-  mongoURI_pro: 'mongodb+srv://ebind:ebind@cluster0.wjcfo.mongodb.net/ebind?retryWrites=true&w=majority',
-}
